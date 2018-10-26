e7285f1fd51c72c354bc76694fa5afd8e2d502d7
test111 <yyylx666@outlook.com>
2018-10-26 11:08:51 +0800

    add articles and notes

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,63 +1,75 @@
 module.exports = {
-    title: '网站标题',
-    description: '网站描述a',
+    title: 'Viola\'s Blog',
+    description: 'Welcome to my site',
     // 注入到当前页面的 HTML <head> 中的标签
     head: [
         ['link', {
             rel: 'icon',
             href: '/favicon.ico'
-        }], // 增加一个自定义的 favicon(网页标签的图标)
+        }], 
     ],
-    base: '/blog/', // 这是部署到github相关的配置 下面会讲 https://violaY33.github.io/blog/
+    base: '/blog/',
     markdown: {
-        lineNumbers: true // 代码块显示行号
+        lineNumbers: true
     },
     themeConfig: {
-        sidebarDepth: 2, // e'b将同时提取markdown中h2 和 h3 标题，显示在侧边栏上。
-        lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间
-        nav: [{
-                text: '前端算法',
-                link: '/algorithm/'
-            }, // 内部链接 以docs为根目录
+        sidebarDepth: 2, 
+        lastUpdated: 'Last Updated', 
+        nav: [
             {
-                text: '博客',
-                link: 'http://obkoro1.com/'
-            }, // 外部链接
-            // 下拉列表
+                text: '文章',
+                link: '/article/'
+            },
             {
-                text: 'GitHub',
-                items: [{
-                        text: 'GitHub地址',
-                        link: 'https://github.com/OBKoro1'
-                    },
+                text: '笔记',
+                link: '/note/'
+            },
+            {                
+                text: '项目',
+                link: '/project/'
+                
+            },
+            {                
+                text: '其他',
+                items: [
+
                     {
-                        text: '算法仓库',
-                        link: 'https://github.com/OBKoro1/Brush_algorithm'
+                        text: '资源',
+                        link: '/resource/'
                     }
                 ]
+                
+            },
+            {
+                text: 'GitHub',
+                link: 'https://github.com/violaY33'
             }
         ],
         sidebar: {
             // docs文件夹下面的accumulate文件夹 文档中md文件 书写的位置(命名随意)
-            '/accumulate/': [
-                '/accumulate/', // accumulate文件夹的README.md 不是下拉框形式
+            '/article/': [
                 {
-                    title: '侧边栏下拉框的标题1',
+                    title: '文章',
                     children: [
-                        '/accumulate/js/post1',
-                        '/accumulate/js/post2', // 以docs为根目录来查找文件 
-                        // 上面地址查找的是：docs>accumulate>JS>test.md 文件
-                        // 自动加.md 每个子选项的标题 是该md文件中的第一个h1/h2/h3标题
+                        '/article/1',
+                        '/article/2',
+                        '/article/3',
+                        '/article/4',
+                        '/article/5',
+                        '/article/6',
+                        '/article/7',
+                        '/article/8',
+                        '/article/9',
+                        '/article/10'
                     ]
                 }
             ],
-            // docs文件夹下面的algorithm文件夹 这是第二组侧边栏 跟第一组侧边栏没关系
-            '/algorithm/': [
-                '/algorithm/',
+            '/note/': [
                 {
-                    title: '第二组侧边栏下拉框的标题1',
+                    title: '读书笔记',
                     children: [
-                        '/algorithm/simple/test'
+                        '/note/1',
+                        '/note/2',
                     ]
                 }
             ]
